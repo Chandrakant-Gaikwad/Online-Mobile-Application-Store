94bb22e685d0c3f5e8fd4c254dcbe66b89c85c19
Chandrakant Gaikwad <147988401+Chandrakant-Gaikwad@users.noreply.github.com>
2025-06-22 19:52:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+<script>
+  document.getElementById('mobile-menu').addEventListener('click', function() {
+    document.getElementById('nav-links').classList.toggle('active')});
+
+    document.querySelector('a').addEventListener('click', function(e) {
+  e.preventDefault();
+});
+
+</script>
